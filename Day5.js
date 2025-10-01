81713906b996aa76459da3c0ce5bef5cab3cd3f2
Khalaf-16 <bolabasirat9@gmail.com>
2025-10-01 19:58:12 +0100

    Started flow control, operators used, fundamentals

---
+++ Day5.js
@@ -0,0 +1,40 @@
+// Flow control is a fundamental concept in programming that allows you to dictate the order in which code is executed based on certain conditions or loops. 
+// 1. if-else statements. e.g: 
+    //   if(condition){
+    //      code to be executed if condition is true
+    //   } else {
+    //      code to be executed if condition is false
+    //   }
+// 2. if-else-else if statements. e.g:
+    //   if(condition1){
+    //      code to be executed if condition1 is true
+    //   } else if(condition2){
+    //      code to be executed if condition2 is true
+    //   } else {
+    //      code to be executed if both condition1 and condition2 are false
+    //   }
+// 3. switch-case statements
+// 4. tenary flow controls
+
+// flow contol uses:
+// # Comparative operators:
+// 1. Equality operators - 1. weak: '=='  2. stict: '==='
+// 2. Non equality operator - 1. weak: '!=' 2. strict: '!=='
+// 3. Less than - '<'
+// 4. Less than or equal to - '<='
+// 5. Greater than or equal to - '>='
+// 6. Greater than - '>'
+
+// weak only compares values, strict compares both values and data types
+// Example:
+// let a = 5; // number
+// let b = '5'; // string
+// console.log(a == b); // true (weak equality, only compares values)
+// console.log(a === b); // false (strict equality, compares value and type)
+
+// # Logic operators:
+// 1. AND - '&&'
+// 2. OR - '||'
+// 3. NOT - '!'
+// Note that when using and operator and or operator, give priority to and operator.
+
